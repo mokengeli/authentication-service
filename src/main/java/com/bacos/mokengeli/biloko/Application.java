edf259064b711825440d53cb1521ee4ed031f52d
emmanuel <emmanuel.babala@dame-prevoyance.com>
2025-05-11 16:04:45 +0200

    feat: add retry

--- src/main/java/com/bacos/mokengeli/biloko/Application.java
+++ src/main/java/com/bacos/mokengeli/biloko/Application.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableAsync
+@EnableRetry
 public class Application {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+01:00"));
--- src/main/java/com/bacos/mokengeli/biloko/application/service/JtiService.java
+++ src/main/java/com/bacos/mokengeli/biloko/application/service/JtiService.java
@@ -1,13 +1,20 @@
 package com.bacos.mokengeli.biloko.application.service;
 
 import com.bacos.mokengeli.biloko.application.port.JtiPort;
+import jakarta.persistence.OptimisticLockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class JtiService {
     private final JtiPort jtiPort;
@@ -24,7 +31,22 @@ public class JtiService {
         return jtiPort.getActiveJti(employeeNumber, appType);
     }
 
+    @Async
+    @Retryable(
+            value = {DataAccessException.class, OptimisticLockException.class},
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void invalidateSession(UUID jti) {
         jtiPort.invalidateSession(jti);
+    }
+
+    /**
+     * Méthode de secours si les retries ont tous échoué.
+     */
+    @Recover
+    public void recoverInvalidateSession(DataAccessException ex, UUID jti) {
+        // loggez, alertez, ou stockez l’échec pour un traitement manuel ultérieur
+        log.error("Échec répété de l’invalidation JTI=" + jti, ex);
     }
 }
